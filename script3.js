b5b557ce599c5d3df681590e02837f626a41a105
Vadim <example@gmail.com>
2024-10-09 20:35:29 +0300

    first commit

---
+++ script3.js
@@ -0,0 +1,17 @@
+
+
+//alert('hello');
+
+//const result = confirm('Are you here?');
+//console.log(result);
+
+//const answer = +prompt('Вам есть 18?', '18');
+//console.log(answer + 5);
+
+const answers = [];
+
+answers[0] = prompt('name?', '')
+answers[1] = prompt('familia?', '')
+answers[2] = prompt('age?', '')
+
+document.write(answers);
---
+++ task2.js
@@ -0,0 +1,12 @@
+const storeName = "Zoo";
+
+const storeDescription = {
+  budget: 10000,
+  employees: ['Alex', 'Max', 'Make'],
+  products: {
+    'apple': 10,
+    'pinapple': 20 
+  },
+
+  open: true 
+}
